0c96ffa0f33d29977771ec93c7fc18bdba8885fe
liufulong <liufulong@douyu.tv>
2021-08-09 16:10:34 +0800

    optimize render store

--- src/app/component/Pannel/Root/index.tsx
+++ src/app/component/Pannel/Root/index.tsx
@@ -4,6 +4,8 @@ import { useDispatch } from 'react-redux';
 import { addChildNodeAction, deleteNodeAction } from "../../../actions";
 import { IStore } from "../../../../store/root-reducers";
 import { useSelector } from 'react-redux';
+import "./index.less";
+import { v4 as uuidv4 } from 'uuid';
 
 interface IProps {
   node: IXmindNode[]
@@ -14,44 +16,47 @@ interface INodeProps {
 function Node({ node }: INodeProps) {
   const root = useSelector((store: IStore) => store.root);
   const dispatch = useDispatch();
-  return <div>
-      <div>
-        {node.content} x:{node.x}; y:{node.y};id:{node.id}
-        <button onClick={() => {
-          const curX = Number(node?.x) + 1;
-          const deep = Number(node?.deep) + 1;
-          const sameNodes = root.filter((item) => deep === item.deep && node === item.parent);
-          const child = new XmindNode({
-            parent: node,
-            content: `子节点${deep}`,
-            deep: curX,
-            x: curX,
-            y: sameNodes.length + 1
-          });
-          dispatch(addChildNodeAction({
-            newNode: child
-          }))
-        }}>添加节点</button>
-        <button onClick={() => {
-          if (!node.parent) {
-            alert("根节点无法添加同级节点");
-            return;
-          }
-          const curX = Number(node?.x);
-          const curY = Number(node?.y) + 1;
-          const deep = Number(node?.deep);
-          const child = new XmindNode({
-            parent: node.parent,
-            content: `同级节点${deep}`,
-            deep: deep,
-            x: curX,
-            y: curY
-          });
-          dispatch(addChildNodeAction({
-            newNode: child
-          }))
-        }}>添加同级节点</button>
-        <button onClick={() => {
+  return <>
+        <div className={ !node.parent ? "root-node node" : "node"}>
+          {node.content} childrens: {node.children?.length}
+          <p>{node.parent ? `父节点：${node.parent.content}` : null}</p>
+          <button onClick={() => {
+            const curX = Number(node?.x) + 1;
+            const deep = Number(node?.deep) + 1;
+            const id = uuidv4();
+            const sameNodes = root.filter((item) => deep === item.deep && node === item.parent);
+            const child = new XmindNode({
+              parent: node,
+              content: `子节点${id.slice(0, 8)}`,
+              deep: curX,
+              id,
+              x: curX,
+              y: sameNodes.length + 1
+            });
+            dispatch(addChildNodeAction({
+              newNode: child
+            }))
+          }}>添加</button>
+          <button onClick={() => {
+            if (!node.parent) {
+              alert("根节点无法添加同级节点");
+              return;
+            }
+            const curX = Number(node?.x);
+            const curY = Number(node?.y) + 1;
+            const deep = Number(node?.deep);
+            const child = new XmindNode({
+              parent: node.parent,
+              content: `同级节点${deep}`,
+              deep: deep,
+              x: curX,
+              y: curY
+            });
+            dispatch(addChildNodeAction({
+              newNode: child
+            }))
+          }}>同级</button>
+          <button onClick={() => {
           if (!node.parent) {
             alert("根节点无法删除")
             return;
@@ -61,17 +66,21 @@ function Node({ node }: INodeProps) {
               curNode: node
             }
           ))
-        }}>删除节点</button>
-      </div>
-  </div>
+        }}>删除</button>
+        </div>
+        <div className={`${node.id?.slice(0, 8)} children-nodes`}>
+        {
+          node.children?.map((item) =>
+            <Node key={item.id} node={item} />
+          )
+        }
+        </div>
+    </>
 }
+
 function RootNode(root: IProps) {
   const node = root.node;
-  return <div>
-    {
-      node.map((item) => <Node key={item.id} node={item} />)
-    }
-  </div>
+  return <Node node={node[0]} />
 }
 
 export default RootNode;
--- src/app/control/index.ts
+++ src/app/control/index.ts
@@ -10,6 +10,13 @@ function getRootNode(node: IXmindNode) :IXmindNode {
   return getRootNode(node.parent);
 }
 
+/**
+ * 判断node与parentNode是否存在"血缘"关系
+ * 
+ * @param parentNode 父节点
+ * @param node 子节点
+ * @returns boolean
+ */
 function isSameNodeParent(parentNode: IXmindNode, node: IXmindNode): boolean {
   if (!node.parent) return false;
   if (node.parent === parentNode) {
@@ -17,9 +24,11 @@ function isSameNodeParent(parentNode: IXmindNode, node: IXmindNode): boolean {
   }
   return isSameNodeParent(parentNode, node.parent);
 }
+
 export const addNode = function(nodeInfo: IAddChildNodeInfo, store: RootState) {
   const { newNode } = nodeInfo;
   const { root } = store;
+  newNode.parent?.children?.push(newNode);
   root.push(newNode);
   return [...root];
 }
@@ -27,36 +36,44 @@ export const addNode = function(nodeInfo: IAddChildNodeInfo, store: RootState) {
 export const deleteNode = function(nodeInfo: IDeleteNodeInfo, store: RootState) {
   const { curNode } = nodeInfo;
   const { root } = store;
-
-  const deleteRoots = root.filter((item) => {
-    const isSame = isSameNodeParent(curNode, item);
-    console.log(isSame);
-    if (curNode.id === item.id || isSame) {
-      return false;
-    }
-    return true;
-  });
-  console.log('deleteRoots');
-  console.log(deleteRoots);
-  const sameNodes = deleteRoots.filter((item) => {
+  if (!curNode.parent) return [];
+  // 1. 将所有待删除的子节点，及子孙节点进行删除
+  const deleteRoots = deleteNodeForRoots(root, curNode);
+  // 2. 更新当前节点的父节点，父节点children需清除该节点
+  const parentNode = curNode.parent;
+  parentNode.children = parentNode?.children?.filter((item) => item.id !== curNode.id);
+  const updateNodes = deleteRoots.map((item) => item.id === parentNode.id ? parentNode : item);
+  // 3. 缓存当前节点的同级节点
+  const cacheSameNodes = updateNodes.filter((item) => {
     if (item.deep === curNode.deep && item.parent === curNode.parent) {
       return true;
     }
     return false;
   });
-  const newRoots = deleteRoots.filter((item) => {
+  // 5. 重置当前节点的同级节点
+  let deep = 1;
+  const resetSameNodes = cacheSameNodes.map((item) => {
+    item.y = deep++;
+    return item;
+  });
+  // 4. 过滤当前节点的同级节点
+  const newRoots = updateNodes.filter((item) => {
     if (item.parent !== curNode.parent) {
       return true;
     }
     return false;
   });
-  let deep = 1;
-  const resetDeepNodes = sameNodes.map((item) => {
-    item.y = deep++;
-    return item;
-  });
 
-  return [...newRoots, ...resetDeepNodes];
+  return [...newRoots, ...resetSameNodes];
 }
 
 
+function deleteNodeForRoots (root: IXmindNode[], curNode: IXmindNode) {
+  return root.filter((item) => {
+    const isSame = isSameNodeParent(curNode, item);
+    if (curNode.id === item.id || isSame) {
+      return false;
+    }
+    return true;
+  });
+}
